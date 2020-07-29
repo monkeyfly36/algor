f541f51c6a089c5b1fef75ff5d0e6f7982b6e3f9
user <user@userdeMacBook-Air.local>
2020-07-30 00:02:53 +0800

    729

--- 3.链表/linkList.js
+++ 3.链表/linkList.js
@@ -1,14 +1,19 @@
 /* 重要概念: 
   节点(data, next), 无头链表(首节点有data和next), 有头链表(首节点有next无data)
-  方法: append, insert, remove, remove_head, remove_tail, indexOf,
-        get, head, tail, length, isEmpty, clear, print // 13种
+  方法: print, append, insert, remove, remove_head, remove_tail, indexOf,
+        get, head, tail, length, isEmpty, clear  // 13种
 */
 
-function LinkList () {
+export const LinkList = function () {
   var Node = function (data) {
     this.data = data
     this.next = null
   }
+  var length = 0
+  var head = null
+  var tail = null
+
+  
   this.append = (data) => {
     var node = new Node(data)
     if (head === null) { // 空链表
@@ -21,21 +26,28 @@ function LinkList () {
     length ++
     return true
   }
-  this.insert = (data, index) => {
+  this.print = () => {
+    var curNode = head
+    while (curNode) {
+      console.log(curNode.data);
+      curNode = curNode.next
 
+    }
   }
-  this.remove = (index) => {
+  // this.insert = (data, index) => {
 
-  }
-  this.remove_head = () => {
+  // }
+  // this.remove = (index) => {
 
-  }
-  this.remove_tail = () => {
+  // }
+  // this.remove_head = () => {
 
-  }
-  this.indexOf = (data) => {
+  // }
+  // this.remove_tail = () => {
 
-  }
-  
-  
+  // }
+  // this.indexOf = (data) => {
+
+  // }
 }
+
