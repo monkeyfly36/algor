d7125a10ed26d96a6a9d7ac9bdfd8eb35f831d5a
zhifeihe <zhifeihe@deepglint.com>
2020-08-25 19:52:36 +0800

    布隆过滤器

--- 4.bitmap/bitMap.js
+++ 4.bitmap/bitMap.js
@@ -1,4 +1,7 @@
-const BitMap = function (size) {
+// 注：BitMap只能存储表示某个数是否存在，可以用于大数据去重，大数据排序，两个集合取交集
+
+// 题：bitmap实现add和isExit方法
+export const BitMap = function (size) {
     var arr = new Array(size)
     for (let i = 0; i < size; i++) {
         arr[i] = 0
@@ -13,16 +16,15 @@ const BitMap = function (size) {
     this.isExist = function (item) {
         let arrIndex = Math.floor(item / 32)
         let bitIndex = Math.floor(item % 32)
-        console.log(arr[arrIndex] & 1 << bitIndex)
         return ((arr[arrIndex] & 1 << bitIndex) !== 0) ? true : false
     }
 }
 // 测试0-100, 4×32 > 100, 所以size为4
-var bit_map = new BitMap(4);
-var arr = [0, 3, 5, 6, 9, 34, 23, 78, 99];
-for (var i = 0; i < arr.length; i++) {
-    bit_map.add(arr[i]);
-}
-console.log(bit_map.isExist(3));
-console.log(bit_map.isExist(7));
-console.log(bit_map.isExist(78));
+// var bit_map = new BitMap(4);
+// var arr = [0, 3, 5, 6, 9, 34, 23, 78, 99];
+// for (var i = 0; i < arr.length; i++) {
+//     bit_map.add(arr[i]);
+// }
+// console.log(bit_map.isExist(3));
+// console.log(bit_map.isExist(7));
+// console.log(bit_map.isExist(78));
