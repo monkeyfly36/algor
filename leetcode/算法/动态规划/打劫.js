3c1dd17569227a3d1860950158cd74ba5602bc43
zhifeihe <zhifeihe@deepglint.com>
2021-01-15 19:14:19 +0800

    feat: 动态规划-打劫

--- leetcode/算法/动态规划/打劫.js
+++ leetcode/算法/动态规划/打劫.js
@@ -4,7 +4,68 @@
 
   给定一个代表每个房屋存放金额的非负整数数组，计算你 不触动警报装置的情况下 ，一夜之内能够偷窃到的最高金额。
 */
-
 var rob = function(nums) {
+  let dp = []
+  let max = 0
+  dp = [0] // 补0
+  for(let i = 0; i<nums.length; i++){
+      dp[i + 1] = Math.max(nums[i] + max, dp[i]) // 递推公式
+      if(dp[i] > max) {
+          max = dp[i]
+      }
+  }
+  return dp[nums.length]
+}
+
+/*
+  leetcode 198. 打劫II
+  同一, 加所有的房屋都 围成一圈 ，这意味着第一个房屋和最后一个房屋是紧挨着的
+
+  思路: 第一个和最后一个不可能在一组里，所以可以拆成两个数组
+*/
+// 同上
+function getRob(nums) {
+  let dp = []
+  let max = 0
+  dp = [0] // 补0
+  for(let i = 0; i<nums.length; i++){
+      dp[i + 1] = Math.max(nums[i] + max, dp[i]) // 递推公式
+      if(dp[i] > max) {
+          max = dp[i]
+      }
+  }
+  return dp[nums.length]
+}
+var rob = function(nums) {
+  if(nums.length === 1) return nums[0]
+  var num1 = JSON.parse(JSON.stringify(nums)) 
+  num1.splice(nums.length-1, 1)
+  var num2 = JSON.parse(JSON.stringify(nums))
+  num2.splice(0, 1)
+  return Math.max(getRob(num1), getRob(num2))
+}
+
+
+/*
+  leetcode 337. 打劫III
+  二叉树结构
+
+  思路: 动态规划
+*/
+var rob = function(root) {
+  const f = new Map() // 用
+  const g = new Map() // 不用
+  // 辅助函数
+  const dfs = (node) => {
+      if (node === null) {
+          return
+      }
+      dfs(node.left)
+      dfs(node.right)
+      f.set(node, node.val + (g.get(node.left) || 0) + (g.get(node.right) || 0))
+      g.set(node, Math.max(f.get(node.left) || 0, g.get(node.left) || 0) + Math.max(f.get(node.right) || 0, g.get(node.right) || 0))
+  }
   
+  dfs(root)
+  return Math.max(f.get(root) || 0, g.get(root) || 0)
 }
